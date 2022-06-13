2d6306ff98c7bef25230f5fe8c8659d52795242d
Luca Andaloro <luca.andaloro4@gmail.com>
2022-06-13 17:55:52 +0200

    fix immagini

--- database/factories/HouseFactory.php
+++ database/factories/HouseFactory.php
@@ -9,7 +9,7 @@ $factory->define(House::class, function (Faker $faker) {
     return [
         'title' => $faker->sentence,
         'address' => $faker->address,
-        'poster' => "https://picsum.photos/",
+        'poster' => "https://picsum.photos/200",
         'description' => $faker->text,
     ];
 });
